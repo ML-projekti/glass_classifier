4b239adb997699fd79f7b3cf4e0c018d932124bb
Viitanen Lauri <lauri.viitanen@aalto.fi>
2015-04-15 16:28:53 +0300

    Implemented proper kNN train+test code.

--- src/GMM_kmeans_cmp.m
+++ src/GMM_kmeans_cmp.m
@@ -1,14 +1,14 @@
-% 40 cm valkokultakaulaketju koivurunkokorulle.
 data = load('../data/glass_dataset.mat');
 all_data = [data.trainData; data.testData];
 all_labels = [data.trainLabels; data.testLabels];
+num_labels = 1 + max(all_labels) - min(all_labels);
 
 component_counts = 2:10;
 mixtures = zeros(max(component_counts),...
-    1+max(all_labels)-min(all_labels),...
+    num_labels,...
     numel(component_counts));
 likelihoods = ones(size(data.testData, 1),...
-    1+max(all_labels)-min(all_labels),...
+    num_labels,...
     numel(component_counts)) * -123456;
 
 for ii = 1:numel(component_counts)
@@ -18,8 +18,8 @@ for ii = 1:numel(component_counts)
 
         if numel(X) > 0 && size(X, 2) > H
             % EM training :
-            opts.plotlik=1;
-            opts.plotsolution=1;
+            opts.plotlik=0;
+            opts.plotsolution=0;
             opts.maxit=200;
             opts.minChange=0.001;
             opts.minChangeCount=5;
@@ -47,10 +47,27 @@ for ii = 1:numel(component_counts)
     end
 end
 
-predicted_classes = zeros(size(data.testData, 1), numel(component_counts));
+predicted_labels = zeros(size(data.testData, 1), numel(component_counts));
 for i = 1:numel(component_counts)
     for n = 1:size(data.testData, 1)
-        predicted_classes(n, i) = find(...
+        predicted_labels(n, i) = find(...
             likelihoods(n, :, i) == max(likelihoods(n, :, i), [], 2));
     end
 end
+
+cfsnmat_GMM_abs = zeros(6, 6, size(predicted_labels, 2));
+cfsnmat_GMM_rel = zeros(6, 6, size(predicted_labels, 2));
+
+for i = 1:size(predicted_labels, 2)
+    cfsnmat_GMM_abs(:, :, i) = confusionmat(...
+        data.testLabels, predicted_labels(:, i));
+    cfsnmat_GMM_rel(:, :, i) = cfsnmat_GMM_abs(:, :, i) ./ repmat(...
+        sum(cfsnmat_GMM_abs(:, :, i), 2), [1, 6]);
+end
+
+model_kNN = fitcknn(data.trainData, data.trainLabels,...
+    'Distance', 'euclidean', 'NumNeighbors', 1);
+kNN_labels = predict(model_kNN, data.testData);
+
+cfsnmat_kNN_abs = confusionmat(data.testLabels, kNN_labels);
+cfsnmat_kNN_rel = cfsnmat_kNN_abs ./ repmat(sum(cfsnmat_kNN_abs, 2), [1, 6]);
